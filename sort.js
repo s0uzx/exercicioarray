fd849f895df9da30e83d7fd76691687898ba23b8
Sara Souza <158165503+s0uzx@users.noreply.github.com>
2024-03-16 19:48:21 -0300

    utilizei o método 'sort' e expliquei dois exemplos dele

---
+++ sort.js
@@ -0,0 +1,8 @@
+// Crio um array com de sabores de pizza
+const saboresDePizzas = ["Portuguesa", "Lombo", "Calabresa", "Quatro Queijos", "Frango com Catupiry"];
+
+// Ordeno os sabores de pizza em ordem alfabética
+saboresDePizzas.sort();
+
+// Exibo os sabores de pizza de forma ordenada
+console.log(saboresDePizzas); // Saída: ["Calabresa", "Frango com Catupiry", "Lombo", "Portuguesa" "Quatro Queijos"]
