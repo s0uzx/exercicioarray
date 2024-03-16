ae1b8d32e1cdee60e255541efafd3bba92d7c46a
Sara Souza <158165503+s0uzx@users.noreply.github.com>
2024-03-16 19:19:07 -0300

    utilizei o método 'indexOf' e explique 2 exemplos dele

---
+++ indexOf.js
@@ -0,0 +1,19 @@
+// Criei um array com nomes de peças de um pc
+const pcPeças = ['CPU', 'GPU', 'RAM', 'SSD', 'Motherboard']; 
+
+// Exibo o índice da peça 'RAM' na matriz (índice 2 no caso)
+console.log(pcPeças.indexOf('RAM')); 
+
+// Exibo o índice da peça 'Power Supply' no array (índice -1 no caso)
+console.log(pcPeças.indexOf('Power Supply'));
+
+// Outro exemplo:
+
+// Crio um array com nomes de cores
+const corzinhas = ['vermelho', 'azul', 'verde', 'amarelo', 'roxo']; 
+
+// Exibo o índice da cor 'amarelo' no array (índice 3)
+console.log(corzinhas.indexOf('amarelo')); 
+
+// Exibo o índice da cor 'vermelho' no array (índice 0)
+console.log(corzinhas.indexOf('vermelho')); 
