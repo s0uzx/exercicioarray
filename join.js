dea9be91614c2c6bcf43f6ff1f0010f6ff0d40e0
Sara Souza <158165503+s0uzx@users.noreply.github.com>
2024-03-16 20:01:00 -0300

    utilizei o método 'join' e expliquei dois exemplos dele

---
+++ join.js
@@ -0,0 +1,24 @@
+
+// Crio um array com saudações
+const boasVindas = ['Olá', 'Seja', 'Bem-Vindo', '!'];
+
+// Junto os elementos do array em uma string, separando-os por um espaço em branco
+const mensagem = boasVindas.join(' ');
+
+// Exibo a mensagem resultante no console
+console.log(mensagem);
+
+// Ficará assim no console:
+// Olá Seja Bem-Vindo !
+
+// Outro exemplo:
+// Defino um array com as partes da minha pergunta
+const pergunta = ['Tem', 'certeza', 'que', 'está', 'tudo', 'bem', '?'];
+
+// Junto os elementos do array em uma string, separando-os por um espaço em branco
+const msg = pergunta.join(' ');
+
+// Exibo a mensagem resultante no console
+console.log(msg);
+
+// Ficará assim no console: Tem certeza que está tudo bem ?
