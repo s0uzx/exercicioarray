7f27f467444f45e70fe57e2bd4f744fe0bd79a63
Sara Souza <158165503+s0uzx@users.noreply.github.com>
2024-03-16 19:37:43 -0300

    utilizei o método 'reverse' e expliquei 2 exemplos dele.

---
+++ reverse.js
@@ -0,0 +1,24 @@
+// Crio um array com os nomes dos membros do BTS (denovo pq eles são uns queridos :))
+const membrosbangtan = ["Namjoon", "Jin", "Yoongi", "J-Hope", "Jimin", "Taehyung", "Jungkook"];
+
+// Uso o método reverse() para inverter a ordem dos nomes no array
+membrosbangtan.reverse();
+
+// Exibo os nomes dos membros do BTS em ordem inversa
+console.log(membrosbangtan); 
+// Aparecerá assim no console: ["Jungkook", "Taehyung", "Jimin", "J-Hope", "Yoongi", "Jin", "Namjoon"]
+
+
+// Outro exemplo:
+
+// Crio um array com os nomes de algumas músicas do Jimin
+const musicasPjm = ["Promise", "Filter", "Friends", "Serendipity", "Lie", "Like Crazy"];
+
+// Uso o método reverse() para inverter a ordem dos nomes no array
+musicasPjm.reverse();
+
+// Exibo os nomes das músicas do Jimin em ordem inversa
+console.log(musicasPjm);
+
+ // O console ficará assim: ["like Crazyg", "Lie", "Serendipity", "Friends", "Filter", "Promise"]
+
